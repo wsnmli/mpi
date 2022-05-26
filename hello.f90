0291fba5bbbd43399fadb94168ebbead56ccc285
matt <05wsnmli@gmail.com>
2022-05-26 04:59:19 +0100

    fix fortranmpi hello world

--- hello.f90
+++ hello.f90
@@ -1,9 +1,12 @@
 PROGRAM helloworld
 USE MPI
 IMPLICIT NONE
-INTEGER :: rank, comsize, ierr
+INTEGER :: rank, size, ierr
 
 CALL MPI_Init(ierr)
+CALL MPI_COMM_SIZE(MPI_COMM_WORLD, size, ierr)
+CALL MPI_COMM_RANK(MPI_COMM_WORLD, rank, ierr)
 
+PRINT*, "Hello, World from", rank, "out of", size
 CALL MPI_FINALIZE(ierr)
 END PROGRAM helloworld
